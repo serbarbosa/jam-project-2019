99312907fb0209c8bbcfde61cca33a43803ebe0f
serbarbosa <sergiobarbosa@usp.br>
2019-01-26 03:18:49 -0200

    ggj player moving. Fst upload

---
+++ ggjProject/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour{
+    [SerializeField]//aparece no inspector mesmo sendo private
+    private float speed = 7f;
+    [SerializeField]
+    private float mouseSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+    void Start() {
+        motor = GetComponent<PlayerMotor>();
+
+    }
+
+    void Update() {
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov; //transform.right = (1,0,0)
+        Vector3 movVertical = transform.forward * zMov; //transform.right = (0,0,1)
+                                                        //vetor apenas com a direcao do movimento
+        Vector3 velocity = (movHorizontal + movVertical).normalized * speed;
+
+        motor.Move(velocity);
+
+        //Calcular rotacao como 3d vector(apenas ao redor)
+        float yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * mouseSensitivity;
+        motor.Rotate(rotation);
+        
+        //Calcular rotacao da camera como 3d vector
+        float xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 cameraRotation = new Vector3(xRot, 0f, 0f) * mouseSensitivity;
+        motor.RotateCamera(cameraRotation);
+    }
+}
