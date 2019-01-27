7bbad0f13f26b6c2fb652d7cef77bdcb51db05fa
serbarbosa <sergiobarbosa@usp.br>
2019-01-27 04:15:56 -0200

    Added fence collision

---
+++ ggjProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour { 
+
+    public float levelMultiplier = 0.7f;
+    bool gameHasEnded = false;
+
+    public void EndGame() {
+        if (!gameHasEnded) {
+            gameHasEnded = true;
+            //carregar menu principal
+        }
+    }
+
+    public void CompleteLevel() {
+        levelMultiplier *= 1.7f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameHasEnded = false;
+    }
+
+}
